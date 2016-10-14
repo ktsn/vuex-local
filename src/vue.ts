b76610b20c2abbe3203aba0676931a40ab73dc13
ktsn <ktsn55@gmail.com>
2016-10-15 00:54:22 +0900

    Fix module augmentation

--- src/vue.ts
+++ src/vue.ts
@@ -1,8 +1,15 @@
 import * as Vue from 'vue'
+import 'vuex'
 import { LocalModule } from './declarations'
 
 declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     local?: LocalModule
+  }
+}
+
+declare module 'vuex/types/index' {
+  interface ActionContext<S, R> {
+    rootGetters: any
   }
 }
